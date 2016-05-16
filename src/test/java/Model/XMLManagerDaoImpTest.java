02b7dc87f6845da4fb43d235432724bc89c19464
Sleis <Sleis@DESKTOP-34A2Q8A>
2016-05-16 14:56:31 +0200

    XMLManagerDaoImpTest file update

--- src/test/java/Model/XMLManagerDaoImpTest.java
+++ src/test/java/Model/XMLManagerDaoImpTest.java
@@ -22,19 +22,14 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import org.junit.AfterClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.w3c.dom.Document;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
 import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
@@ -43,21 +38,28 @@ import org.xml.sax.SAXException;
  */
 public class XMLManagerDaoImpTest {
 
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+    private static XMLManagerDao instance;
+
+    @BeforeClass
+    public static void setUpClass() {
+        instance = new XMLManagerDaoImp();
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        instance = null;
+    }
+
     /**
      * Test of createPlayers method, of class XMLManagerDaoImp.
      */
     @Test
     public void testCreatePlayers() {
-        XMLManagerDaoImp instance = new XMLManagerDaoImp();
-        Path p = Paths.get(System.getProperty("user.home"), "Documents", "Reversi", "teszt.xml");
-        if (!p.toFile().isFile()) {
-            Path dir = Paths.get(System.getProperty("user.home"), "Documents", "Reversi");
-            dir.toFile().mkdirs();
-            instance.createPlayers(p);
-        }
+        Path p = Paths.get(tempFolder.getRoot().getPath(), "players.xml");
+        instance.createPlayers(p);
         assertTrue(p.toFile().isFile());
-        File file = new File(p.toString());
-        file.delete();
 
     }
 
@@ -68,14 +70,8 @@ public class XMLManagerDaoImpTest {
      */
     @Test
     public void testReadPlayers() throws SAXException {
-        XMLManagerDaoImp instance = new XMLManagerDaoImp();
-        Path p = Paths.get(System.getProperty("user.home"), "Documents", "Reversi", "teszt.xml");
-        if (!p.toFile().isFile()) {
-            Path dir = Paths.get(System.getProperty("user.home"), "Documents", "Reversi");
-            dir.toFile().mkdirs();
-            instance.createPlayers(p);
-
-        }
+        Path p = Paths.get(tempFolder.getRoot().getPath(), "players.xml");
+        instance.createPlayers(p);
         Player test = new Player("Dani", 10);
         instance.addNewPlayer(p, test);
         List<Element> list = instance.readPlayers(p);
@@ -85,9 +81,6 @@ public class XMLManagerDaoImpTest {
         assertEquals(test.getName(), list.get(0).getElementsByTagName("name").item(0).getTextContent());
         assertEquals(test.getPoints(), Integer.parseInt(list.get(0).getElementsByTagName("points").item(0).getTextContent()));
         assertEquals(test.getLocalDate(), LocalDate.parse(list.get(0).getElementsByTagName("date").item(0).getTextContent()));
-        File file;
-        file = p.toFile();
-        file.delete();
     }
 
     /**
@@ -95,13 +88,8 @@ public class XMLManagerDaoImpTest {
      */
     @Test
     public void testSortPlayersByScore() {
-        XMLManagerDaoImp instance = new XMLManagerDaoImp();
-        Path p = Paths.get(System.getProperty("user.home"), "Documents", "Reversi", "teszt.xml");
-        if (!p.toFile().isFile()) {
-            Path dir = Paths.get(System.getProperty("user.home"), "Documents", "Reversi");
-            dir.toFile().mkdirs();
-            instance.createPlayers(p);
-        }
+        Path p = Paths.get(tempFolder.getRoot().getPath(), "players.xml");
+        instance.createPlayers(p);
         Player test1 = new Player("Dani", 10);
         Player test2 = new Player("Pisti", 8);
         Player test3 = new Player("Gábor", 12);
@@ -111,17 +99,9 @@ public class XMLManagerDaoImpTest {
         List<Element> list = instance.readPlayers(p);
         list = instance.sortPlayersByScore(list);
         assertEquals("Gábor", list.get(0).getElementsByTagName("name").item(0).getTextContent());
-        assertEquals(12, list.get(0).getElementsByTagName("poins").item(0).getTextContent());
-        File file = p.toFile();
+        assertEquals(12, Integer.parseInt(list.get(0).getElementsByTagName("points").item(0).getTextContent()));
+        File file = new File(p.toString());
         file.delete();
-
-    }
-
-    /**
-     * Test of addNewPlayer method, of class XMLManagerDaoImp.
-     */
-    @Test
-    public void testAddNewPlayer() {
 
     }
 
