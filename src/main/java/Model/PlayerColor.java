82fc9bfb976d3092279e74668af45702ff9d7e73
Sleis <Sleis@DESKTOP-D4FPN57>
2016-05-20 15:39:12 +0200

    Dokumentáció frissités

--- src/main/java/Model/PlayerColor.java
+++ src/main/java/Model/PlayerColor.java
@@ -21,48 +21,82 @@ package Model;
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
-
 import java.util.Objects;
 
 /**
+ * A játékosok színeit tartalmazó osztály.
  *
  * @author Iványi Dániel
  */
 public class PlayerColor {
+
+    /**
+     * A soron következő játékos szine.
+     */
     private String yourColor;
+    /**
+     * Az ellenfél szine.
+     */
     private String enemyColor;
 
+    /**
+     * Létrehoz egy <code>PlayerColor</code> objektumot üres
+     * <code>yourColor</code> és <code>enemyColor</code> tulajdonsággal.
+     */
     public PlayerColor() {
     }
-    
 
+    /**
+     * Létrehoz egy <code>PlayerColor</code> objektumot <code>yourColor</code>
+     * és <code>enemyColor</code> tulajdonsággal.
+     *
+     * @param yourColor a soron következő játékos szine
+     * @param enemyColor az ellenfél szine
+     */
     public PlayerColor(String yourColor, String enemyColor) {
         this.yourColor = yourColor;
         this.enemyColor = enemyColor;
     }
-
+    /**
+     * Visszaadja az objektum <code>yourColor</code> tulajdonságát.
+     * 
+     * @return az objektum <code>yourColor</code> tulajdonsága
+     */
     public String getYourColor() {
         return yourColor;
     }
-
+    /**
+     * Visszaadja az objektum <code>enemyColor</code> tulajdonságát.
+     * 
+     * @return az objektum <code>enemyColor</code> tulajdonsága
+     */
     public String getEnemyColor() {
         return enemyColor;
     }
-
+    /**
+     * Beállítja az objektum <code>yourColor</code> tulajdonságát.
+     * 
+     * @param yourColor az objektum <code>yourColor</code> tulajdonsága
+     */
     public void setYourColor(String yourColor) {
         this.yourColor = yourColor;
     }
-
+    /**
+     * Beállítja az objektum <code>enemyColor</code> tulajdonságát.
+     * 
+     * @param enemyColor az objektum <code>enemyColor</code> tulajdonsága
+     */
     public void setEnemyColor(String enemyColor) {
         this.enemyColor = enemyColor;
     }
-
+    @SuppressWarnings("javadocmethod")
     @Override
     public int hashCode() {
         int hash = 7;
         return hash;
     }
-
+    
+    @SuppressWarnings("javadocmethod")
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -83,6 +117,5 @@ public class PlayerColor {
         }
         return true;
     }
-    
-    
+
 }
