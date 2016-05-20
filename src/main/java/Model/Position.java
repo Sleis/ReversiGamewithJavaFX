73dfe7cd2ea686da842ebb20422f0a4f8da2283a
Sleis <Sleis@DESKTOP-34A2Q8A>
2016-05-20 17:30:06 +0200

    Hozzáadva a Position.java osztály dokumentációja.

--- src/main/java/Model/Position.java
+++ src/main/java/Model/Position.java
@@ -21,38 +21,75 @@ package Model;
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
-
 /**
+ * Adott cella pozíciót tartalmazó osztály.
  *
  * @author Iványi Dániel
  */
 public class Position {
+
+    /**
+     * A cella sorszáma.
+     */
     private int sor;
+    /**
+     * A cella oszlopszáma.
+     */
     private int oszlop;
 
+    /**
+     * Létrehoz egy <code>Position</code> objektumot üres <code>sor</code> és
+     * <code>oszlop</code> tulajdonsággal.
+     */
     public Position() {
     }
-    
+
+    /**
+     * Létrehoz egy <code>Position</code> objektumot adott <code>sor</code> és
+     * <code>oszlop</code> tulajdonsággal.
+     *
+     * @param sor az objektum <code>sor</code> tulajdonsága
+     * @param oszlop az objektum <code>oszlop</code> tulajdonsága
+     */
     public Position(int sor, int oszlop) {
         this.sor = sor;
         this.oszlop = oszlop;
     }
 
+    /**
+     * Visszaadja az objektum <code>sor</code> tulajdonságát.
+     *
+     * @return az objektum <code>sor</code> tulajdonsága
+     */
     public int getSor() {
         return sor;
     }
 
+    /**
+     * Visszaadja az objektum <code>oszlop</code> tulajdonságát.
+     *
+     * @return az objektum <code>oszlop</code> tulajdonsága
+     */
     public int getOszlop() {
         return oszlop;
     }
 
+    /**
+     * Beállítja az objektum <code>sor</code> tulajdonságát.
+     *
+     * @param sor az objektum <code>sor</code> tulajdonsága
+     */
     public void setSor(int sor) {
         this.sor = sor;
     }
 
+    /**
+     * Beállítja az objektum <code>oszlop</code> tulajdonságát.
+     *
+     * @param oszlop az objektum <code>oszlop</code> tulajdonsága
+     */
     public void setOszlop(int oszlop) {
         this.oszlop = oszlop;
     }
-    
-    
+
 }
