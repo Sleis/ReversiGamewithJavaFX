f11e7067e8b6717012afe6385235cd2b96c6647f
Sleis <Sleis@DESKTOP-34A2Q8A>
2016-05-20 17:42:11 +0200

    Hozzáadva a XMLManagerDao.java osztály dokumentációja.

--- src/main/java/Model/XMLManagerDao.java
+++ src/main/java/Model/XMLManagerDao.java
@@ -21,23 +21,51 @@ package Model;
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
-
 import java.nio.file.Path;
 import java.util.List;
 import org.w3c.dom.Element;
 
 /**
+ * Az osztály az XML fájlon végezhető műveletek definiálja.
  *
  * @author Iványi Dániel
  */
 public interface XMLManagerDao {
 
+    /**
+     * Létrehozza a <code>path</code> útvonalon a players.xml fájlt.
+     *
+     * @param path az útvonal, ahol létrehozzuk az XML fájlt
+     */
     public void createPlayers(Path path);
 
+    /**
+     * Visszaadja a listát, ami az XML fájlban lévő adatokat tartalmazza.
+     *
+     * @param path az XML fájl elérési útvonala
+     *
+     * @return egy lista, ami az XML fájlból kiolvasott adatokat tartalmazza
+     */
     public List<Element> readPlayers(Path path);
 
+    /**
+     * Visszaad egy listát, amiben a <code>lista</code> elemei rendezve vannak
+     * pontszám szerint.
+     *
+     * @param lista egy lista a játékosok adataival
+     *
+     * @return egy lista amiben a <code>lista</code> elemei rendezve vannak
+     * pontszám szerint
+     */
     public List<Element> sortPlayersByScore(List<Element> lista);
 
+    /**
+     * Hozzáad egy új játékost az XML fájlhoz.
+     *
+     * @param path az XML fájl elérési útvonala
+     *
+     * @param jatekos a játékos adatai, amit az XML fájlhoz adunk
+     */
     public void addNewPlayer(Path path, Player jatekos);
 
 }
